5ed0856d3f2c81f2b5696e9d3d2467a3dd9366dd
Lynn <kjarli@gmail.com>
2024-08-13 18:08:41 +0200

    Updated for 11.0.2

--- Changelog.lua
+++ Changelog.lua
@@ -2,6 +2,12 @@ local _, Namespace = ...
 
 Namespace.Changelog = {
     {
+        version = '11.0.2-50',
+        improvements = {
+            'Updated TOC for 11.0.2',
+        },
+    },
+    {
         version = '11.0.0-49',
         improvements = {
             'The War Within update for 11.0.0',
